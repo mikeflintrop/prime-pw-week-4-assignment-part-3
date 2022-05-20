a495d8e9e32b97a99353449a546190016c9162b5
Mike Flintrop <mike.flintrop@gmail.com>
2022-05-20 14:26:28 -0500

    1st time through part 3, working on stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,34 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = [];
+function addItem(item) {
+basket.push(item);
+return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(basket) {
+    for (let i = 0; i < basket.length; i++) {
+    return (basket[i]);  
+    }
+}
+console.log(listItems(basket));
+
+function empty(basket) {
+    for (let i = 0; i < basket.length; i++) {
+    return (basket = []);  
+    }
+}
+console.log(empty(basket));
+
+//----------------------------------------------------------------
+const maxItems = 5;
+
+function isFull(number) {
+    return (number >= maxItems ? true : false);
+}
+console.log(isFull(5));
+
